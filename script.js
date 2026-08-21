33b7f90dcf49a8aea0da6f7502ea52d511162b83
Hexa-Programmer <harsukh669@gmail.com>
2026-08-21 15:34:00 +0530

    Added application "Notes"

---
+++ script.js
@@ -0,0 +1,71 @@
+function updateClock() {
+  const now = new Date();
+  document.getElementById("timeElement").textContent = now.toLocaleTimeString([], { 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      second: '2-digit' 
+  });
+}
+updateClock();
+setInterval(updateClock, 1000);
+
+var windowElement = document.querySelector("#window");
+var windowCloseBtn = document.querySelector("#windowclose");
+var windowOpenBtn = document.querySelector("#windowopen");
+
+function closeWindow(element) {
+  element.style.display = "none";
+}
+
+function openWindow(element) {
+  element.style.display = "flex";
+}
+
+windowCloseBtn.addEventListener("click", function () {
+  closeWindow(windowElement);
+});
+
+windowOpenBtn.addEventListener("click", function () {
+  openWindow(windowElement);
+});
+
+dragElement(windowElement);
+
+function dragElement(element) {
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+  if (document.getElementById(element.id + "header")) {
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    initialX = e.clientX;
+    initialY = e.clientY;
+    document.onmouseup = stopDragging;
+    document.onmousemove = moveElement;
+  }
+
+  function moveElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
